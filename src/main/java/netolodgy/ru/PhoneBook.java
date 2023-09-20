a90aff34e6cd6de3e6911ecc9d08bf5b53442557
Dmitriy Nazarov <difat1@inbox.ru>
2023-09-20 20:12:14 +0300

    Stub and tests for add

--- src/main/java/netolodgy/ru/PhoneBook.java
+++ src/main/java/netolodgy/ru/PhoneBook.java
@@ -1,4 +1,14 @@
 package netolodgy.ru;
 
+import java.util.TreeSet;
+
 public class PhoneBook {
+    protected TreeSet phoneBookList = new TreeSet();
+
+    public int add(String name, int number) {
+        return 0;
+    }
+
 }
+
+
--- src/test/java/netolodgy/ru/PhoneBookTest.java
+++ src/test/java/netolodgy/ru/PhoneBookTest.java
@@ -1,7 +1,13 @@
 package netolodgy.ru;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 
 class PhoneBookTest {
-
+    PhoneBook phoneBook = new PhoneBook();
+    @Test
+    void validatePhoneBookAdd() {
+        Integer result  = phoneBook.add("Pit", 89952444);
+        Assertions.assertEquals(1, result);
+    }
 }
