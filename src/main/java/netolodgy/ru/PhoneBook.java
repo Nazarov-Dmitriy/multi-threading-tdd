65ea5fd36cdfbd211d8814cd0c4755d725148a8b
Dmitriy Nazarov <difat1@inbox.ru>
2023-09-20 20:20:04 +0300

    add implemented

--- src/main/java/netolodgy/ru/PhoneBook.java
+++ src/main/java/netolodgy/ru/PhoneBook.java
@@ -1,14 +1,15 @@
 package netolodgy.ru;
 
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class PhoneBook {
-    protected TreeSet phoneBookList = new TreeSet();
+    protected TreeMap<String, Integer> phoneBookList = new TreeMap<>();
 
     public int add(String name, int number) {
-        return 0;
+        phoneBookList.put(name, number);
+        return phoneBookList.size();
     }
-
 }
 
 
